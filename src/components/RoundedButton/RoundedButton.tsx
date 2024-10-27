439cee3e155883b44ac1c9e1dcc9694c14da389d
Paulo Cesar Hernández Rosado <paulocesarhero@gmail.com>
2024-10-26 19:42:39 -0600

    add jsdoc RoundedButton.tsx

--- src/components/RoundedButton/RoundedButton.tsx
+++ src/components/RoundedButton/RoundedButton.tsx
@@ -1,14 +1,31 @@
 import React from "react";
-import { Pressable, Text, View } from "react-native";
-import { RoundedButtonProps } from "./types";
-import { RoundedButtonStyle } from "./RoundedButton.style";
+import {Pressable, Text, View} from "react-native";
+import {RoundedButtonProps} from "./types";
+import {RoundedButtonStyle} from "./RoundedButton.style";
+
+/**
+ * Un componente de botón redondeado personalizable para aplicaciones en React Native.
+ *
+ * @component
+ * @param {RoundedButtonProps} props - Propiedades para configurar el RoundedButton.
+ *
+ *
+ * @example
+ * // Ejemplo de uso:
+ * <RoundedButton
+ *   color="#fff"
+ *   text="Aceptar"
+ *   style={{ margin: 10 }}
+ *   onPress={() => console.log("Botón presionado")}
+ * />
+ */
 
 const RoundedButton: React.FC<RoundedButtonProps> = ({
   color,
   text,
   style,
   ...restButtonProps
-}) => {
+}: RoundedButtonProps) => {
   return (
     <View style={style}>
       <Pressable
--- src/components/RoundedButton/types.d.ts
+++ src/components/RoundedButton/types.d.ts
@@ -1,4 +1,13 @@
-import { ColorValue, PressableProps, StyleProp } from "react-native";
+import {ColorValue, PressableProps, StyleProp, ViewStyle} from "react-native";
+/**
+ * Propiedades para el componente RoundedButton.
+ *
+ * @interface RoundedButtonProps
+ * @extends {PressableProps}
+ * @property {string | ColorValue} [color] - Color de fondo del botón.
+ * @property {string} [text] - Texto que se mostrará dentro del botón.
+ * @property {StyleProp<ViewStyle>} [style] - Estilos adicionales para el contenedor del botón.
+ */
 export interface RoundedButtonProps extends PressableProps {
   color?: string | ColorValue;
   text?: string;
