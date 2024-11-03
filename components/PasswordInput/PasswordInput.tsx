075d8b53ddf17547737565e402c3290fcf606974
Paulo Cesar Hernández Rosado <paulocesarhero@gmail.com>
2024-11-03 15:11:18 -0600

    fix PasswordInput.tsx and add Jsdoc

--- components/PasswordInput/PasswordInput.tsx
+++ components/PasswordInput/PasswordInput.tsx
@@ -2,7 +2,26 @@ import React, { useState } from "react";
 import { Pressable, TextInput, View } from "react-native";
 import { PasswordInputStyle } from "./PasswordInput.style";
 import { PasswordInputProps } from "./PasswordInputProps";
-import { NoVisibleEye, VisibleEye } from "../../src/icons/icons"; // Importar los íconos
+import { NoVisibleEye, VisibleEye } from "../../src/icons/icons";
+import {usePlaceholderColor} from "../hooks/colorHook";
+/**
+ * Componente de entrada de contraseña que permite mostrar y ocultar el texto ingresado.
+ * Incluye un ícono interactivo para alternar la visibilidad de la contraseña.
+ *
+ * @component
+ * @param {PasswordInputProps} props - Las propiedades del componente de entrada de contraseña.
+ * @returns {React.ReactElement} Un campo de entrada de contraseña con un ícono de visibilidad.
+ *
+ * @example
+ * // Uso en un formulario
+ *
+ * const MyFormComponent = () => (
+ *   <PasswordInput
+ *     placeholder="Ingrese su contraseña"
+ *     onChangeText={(text) => console.log(text)}
+ *   />
+ * );
+ */
 
 const PasswordInput: React.FC<PasswordInputProps> = (props) => {
   const [isPasswordVisible, setPasswordVisibility] = useState<boolean>(false);
@@ -12,11 +31,13 @@ const PasswordInput: React.FC<PasswordInputProps> = (props) => {
   };
 
   const EyeIcon = isPasswordVisible ? NoVisibleEye : VisibleEye;
+  const colorPlaceholder = usePlaceholderColor(); // Usa el color definido en el hook
 
   return (
     <View style={PasswordInputStyle.container}>
       <TextInput
         autoComplete="password"
+        placeholderTextColor={colorPlaceholder}
         style={PasswordInputStyle.input}
         secureTextEntry={!isPasswordVisible} // Contraseña oculta por defecto
         placeholder="Password"
