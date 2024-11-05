9174a110f4e2981eaf8f50b42c5711b64c432dfe
Paulo Cesar Hernández Rosado <paulocesarhero@gmail.com>
2024-11-04 18:42:46 -0600

    add loginform logic

---
+++ components/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import LoginForm from "./LoginForm";
+import { action } from "@storybook/addon-actions";
+import { View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+export default {
+  title: "Components/LoginForm",
+  component: LoginForm,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "Componente de formulario de inicio de sesión que permite a los usuarios ingresar su correo electrónico y contraseña, y tiene un botón para enviar los datos de inicio de sesión.",
+      },
+    },
+  },
+};
+
+// Ejemplo de historia básica
+export const Basic = () => (
+    <LoginForm
+      email=""
+      onEmailChange={action("onEmailChange")}
+      onPasswordChange={action("onPasswordChange")}
+      onLoginPress={action("email")}
+    />
+);
+
+// Ejemplo de historia básica
+export const Basic2 = () => {
+  // Estado local para manejar el email y la contraseña
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+      <LoginForm
+        email={email} // Estado del email
+        password={password} // Estado de la contraseña
+        onEmailChange={(newEmail) => {
+          setEmail(newEmail); // Actualiza el email en el estado
+          action("onEmailChange")(newEmail); // Captura la acción
+        }}
+        onPasswordChange={(newPassword) => {
+          setPassword(newPassword); // Actualiza la contraseña en el estado
+          action("onPasswordChange")(newPassword); // Captura la acción
+        }}
+        onLoginPress={() => {
+          action("onLoginPress")({ email, password });
+        }}
+      />
+  );
+};
+
+// Historia que muestra el estado de carga
+export const LoadingState = () => (
+    <LoginForm
+      email="test@example.com"
+      password="password123"
+      onEmailChange={action("onEmailChange")}
+      onPasswordChange={action("onPasswordChange")}
+      onLoginPress={action("onLoginPress")}
+      loading={true}
+    />
+);
+
--- components/LoginForm/LoginForm.tsx
+++ components/LoginForm/LoginForm.tsx
@@ -1,10 +1,14 @@
 import {Text, View} from "react-native";
 import React from "react";
-import {LoginFormStyle} from "./LoginForm.style";
+import { LoginFormStyle } from "./LoginForm.style";
 import RoundedButton from "../RoundedButton/RoundedButton";
 import PasswordInput from "../PasswordInput/PasswordInput";
 import EmailInput from "../EmailInput/EmailInput";
-import {LoginFormProps} from "./types";
+import { LoginFormProps } from "./types";
+import { LinearGradient } from "expo-linear-gradient";
+import {LoginPageStyle} from "../../src/screens/Login/LoginPage.style";
+import { Image } from "expo-image";
+
 
 /**
  * Componente de formulario de inicio de sesión.
@@ -35,23 +39,36 @@ const LoginForm: React.FC<LoginFormProps> = ({
   loading = false,
 }: LoginFormProps) => {
   return (
-    <View style={LoginFormStyle.container}>
-      <Text style={LoginFormStyle.label}>Correo electrónico</Text>
-      <EmailInput value={email} onEmailTextChange={onEmailChange} />
-      <Text style={LoginFormStyle.label}>Contraseña</Text>
-      <PasswordInput
-        placeholder={"Contraseña"}
-        value={password}
-        onChangeText={onPasswordChange}
+    <LinearGradient
+      style={LoginFormStyle.container}
+      colors={["#024D76", "#3b5998", "#54AD94"]}
+    >
+      <Image
+          source={require("./logo.png")}
+          style={LoginFormStyle.image}
+          contentFit={"contain"}
       />
-      <RoundedButton
-        style={{ marginVertical: 30 }}
-        color={ "#3498db"}
-        text={loading ? "Loading..." : buttonText}
-        onPress={onLoginPress}
-        disabled={loading}
-      />
-    </View>
+      <View style={LoginFormStyle.form}>
+        <Text style={LoginFormStyle.label}>Correo electrónico</Text>
+        <EmailInput value={email} onEmailTextChange={onEmailChange} />
+        <Text style={LoginFormStyle.label}>Contraseña</Text>
+        <PasswordInput
+            placeholder={"Contraseña"}
+            value={password}
+            onChangeText={onPasswordChange}
+        />
+        <RoundedButton
+            style={{ marginVertical: 30 }}
+            color={"#090909"}
+            textSize={25}
+            text={loading ? "Loading..." : buttonText}
+            onPress={onLoginPress}
+            disabled={loading}
+        />
+
+      </View>
+
+    </LinearGradient>
   );
 };
 
