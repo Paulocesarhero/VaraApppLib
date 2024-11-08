cc55e9df4f2e15cd44e40f56d2b136927f152f70
Javier Durán Torres <javier_dt30@outlook.com>
2024-11-08 11:03:48 -0600

    Material card (#1)
    * add MaterialCard.sotries.tsx
    
    * add MaterialCard
    
    * Cambios dentro de MaterialCard
    
    * Cambios dentro de la documentación de MaterialCard junto con el cambio del nombre del prop icon, ahora es leftComponent

---
+++ components/MaterialCard/MaterialCard.tsx
@@ -0,0 +1,36 @@
+import { MaterialCardProps } from "./types";
+import { Pressable, Text, View } from "react-native";
+import { MaterialCardStyles } from "./MaterialCard.style";
+
+/**
+ * Componente de Card que muestra un texto y un ícono.
+ * Permite al usuario dar click.
+ *
+ * @component
+ * @param {MaterialCardProps} props - Las propiedades del componente.
+ * @example
+ * <MaterialCard
+ *   label={"Información"}
+ *   leftComponent={<AntDesign name="infocirlceo" size={24} color="black" />}
+ *   viewStyle={{backgroundColor: '#FFF'}}
+ *   onPress={() => console.log("Press") }
+ * />
+ */
+
+const MaterialCard: React.FC<MaterialCardProps> = ({
+  leftComponent,
+  label,
+  viewStyle,
+  ...props
+}) => {
+  return (
+    <Pressable {...props}>
+      <View style={[MaterialCardStyles.container, viewStyle]}>
+        <View style={MaterialCardStyles.containerImage}>{leftComponent}</View>
+        <Text style={MaterialCardStyles.containerLabelText}>{label}</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+export default MaterialCard;
