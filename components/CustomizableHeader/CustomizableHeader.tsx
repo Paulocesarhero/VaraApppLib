d3526e086b60390895edccef1b12ace59a62024d
Paulo Cesar Hernández Rosado <paulocesarhero@gmail.com>
2024-11-09 17:24:41 -0600

    Refact customizable headre

--- components/CustomizableHeader/CustomizableHeader.tsx
+++ components/CustomizableHeader/CustomizableHeader.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, ViewStyle } from "react-native";
+import { View } from "react-native";
 import { CustomizableHeaderStyles } from "./CustomizableHeader.style";
 import { CustomizableHeaderProps } from "./types";
 
