d10450d60e33a8f5b787293a260405cad38e46a4
Paulo Cesar Hernández Rosado <paulocesarhero@gmail.com>
2024-12-06 14:46:16 -0600

    add jsDoc to PhotoPreview.tsx

--- components/Camera/PhotoPreview.tsx
+++ components/Camera/PhotoPreview.tsx
@@ -1,24 +1,21 @@
 import React from "react";
-import { Pressable, View } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
-import { CameraButtonStyle } from "./CameraButton/CameraButton.style";
+import { View } from "react-native";
 import { Image } from "expo-image";
+import { CameraButtonStyle } from "./CameraButton/CameraButton.style";
 
-/**
- * Componente que muestra una vista previa de la foto capturada y un botón para volver a tomarla.
- *
- * @component
- */
 const PhotoPreview: React.FC<{
   photoUri: string | null;
   styleCamerPreview?: any;
-}> = ({ photoUri, styleCamerPreview }) => (
-  <View>
-    <Image
-      source={{ uri: photoUri }}
-      style={[CameraButtonStyle.image, styleCamerPreview]}
-      contentFit={"scale-down"}
-    />
-  </View>
-);
+}> = ({ photoUri, styleCamerPreview }) => {
+  return (
+    <View>
+      <Image
+        source={{ uri: photoUri }}
+        style={[CameraButtonStyle.image, styleCamerPreview]}
+        contentFit={"scale-down"}
+      />
+    </View>
+  );
+};
+
 export default PhotoPreview;
